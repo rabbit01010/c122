d64beea3b3243491b792df100ddea22c84aeaca8
rabbit01010 <147127241+rabbit01010@users.noreply.github.com>
2024-03-24 20:18:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+    canvas = createCanvas(740, 400);
+    canvas.position(0, 250);
+    video = createCapture(VIDEO);
+    video.size(740, 500);
+}
+
+function draw() {
+    fill(1386, 0, 0);
+    stroke(1386, 128, 0);
+    circle(690, 50, 70);
+    circle(50, 50, 70);
+    circle(690, 350 ,70);
+    circle(50, 350 ,70);
+    
+    fill(0, 128, 0);
+    stroke(1386, 1268, 0);
+    rect(32, 84, 25, 233);
+    rect(680, 84, 25, 233);
+    rect(85, 40, 570, 25);
+    rect(85, 340, 570, 25);
+}
+
+function take_snapshot(){
+    save('student_name.png');
+}
+
